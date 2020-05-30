d05bc9448048edc302ab8085ff3730de39888e25
wufeng <wufeng@fclassroom.com>
2020-05-30 16:59:07 +0800

    feat(*): 接口restful化
    1.合并、重组当前所有api接口，转为restful规则：role/getRoleInfoList -> roleInfoList.json:get

--- route/api/management/role_all_permission.py
+++ route/api/management/role_all_permission.py
@@ -6,6 +6,6 @@ from flask_restful import Resource
 from .role_all_permission_restful.get import role_all_permission_get
 
 
-class RolePermission(Resource):
+class RoleAllPermission(Resource):
     def get(self):
         return role_all_permission_get()
