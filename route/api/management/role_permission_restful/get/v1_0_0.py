71919f786385199f7b5a35befbb88bd4de11635d
wufeng <wufeng@fclassroom.com>
2020-05-30 14:17:54 +0800

    feat(*): 接口restful化
    1.合并、重组当前所有api接口，转为restful规则：role/getRolePermission -> rolePermission.json:get

--- route/api/management/role_permission_restful/get/v1_0_0.py
+++ route/api/management/role_permission_restful/get/v1_0_0.py
@@ -1,17 +1,10 @@
 # -*- coding: utf-8 -*-
 
 import flask
-import re
-import json
+import route
 
-from sqlalchemy import func
-
-from handler.api.error import ApiError
-from handler.api.check import ApiCheck
 from handler.log import api_logger
 from handler.pool import mysqlpool
-
-from route.api.management.role import api_management_role
 
 from model.mysql import model_mysql_functioninfo
 from model.mysql import model_mysql_rolepermission
@@ -25,8 +18,13 @@ from model.mysql import model_mysql_rolepermission
 # ----操作
 # 4.于数据库中查询角色配置清单
 # 5.于数据库中查询全量配置清单
-@api_management_role.route('/getRolePermission.json', methods=["post"])
-def get_role_permission():
+@route.check_user
+@route.check_token
+@route.check_auth
+@route.check_get_parameter(
+    ['role_id', int, 1, None]
+)
+def role_permission_get():
     # 初始化返回内容
     response_json = {
         "error_code": 200,
@@ -39,86 +37,8 @@ def get_role_permission():
         }
     }
 
-    # 1.校验传参
-    # 取出请求参数
-    try:
-        request_json = flask.request.json
-    except Exception as e:
-        logmsg = "/getRolePermission.json数据格式化失败，失败原因：" + repr(e)
-        api_logger.error(logmsg)
-        return ApiError.requestfail_error("接口数据异常")
-    else:
-        if request_json is None:
-            return ApiError.requestfail_error("接口数据异常")
-    # 检查必传项是否遗留
-    # mail_address
-    if "mail_address" not in request_json:
-        return ApiError.requestfail_nokey("mail_address")
-    # user_token
-    if "user_token" not in request_json:
-        return ApiError.requestfail_nokey("user_token")
-    # role_id
-    if "role_id" not in request_json:
-        return ApiError.requestfail_nokey("role_id")
-    # 检查通过
-    # 检查必传项内容格式
-    # mail_address
-    if type(request_json["mail_address"]) is not str or len(request_json["mail_address"]) > 100:
-        return ApiError.requestfail_value("mail_address")
-    if re.search("^[0-9a-zA-Z_]{1,100}@fclassroom.com$", request_json["mail_address"]) is None:
-        return ApiError.requestfail_value("mail_address")
-    # record_code
-    if type(request_json["user_token"]) is not str or len(request_json["user_token"]) > 100:
-        return ApiError.requestfail_value("user_token")
-    # role_id
-    if type(request_json["role_id"]) is not int or request_json["role_id"] < 1:
-        return ApiError.requestfail_value("role_id")
-
     # 取出传入参数值
-    requestvalue_mail = request_json["mail_address"]
-    requestvalue_token = request_json["user_token"]
-    requestvalue_roleid = request_json["role_id"]
-
-    # 2.校验账户是否存在
-    userdata = ApiCheck.check_user(requestvalue_mail)
-    if userdata["exist"] is False:
-        return ApiError.requestfail_error("账户不存在")
-    elif userdata["exist"] is True and userdata["userStatus"] == 0:
-        return ApiError.requestfail_error("账户未激活")
-    elif userdata["exist"] is True and userdata["userStatus"] == 1:
-        pass
-    elif userdata["exist"] is True and userdata["userStatus"] == -1:
-        return ApiError.requestfail_error("账户已禁用")
-    else:
-        return ApiError.requestfail_error("账户信息校验异常")
-
-    # 3.校验令牌是否有效
-    tc = ApiCheck.check_token(
-        requestvalue_mail,
-        requestvalue_token
-    )
-    if tc["exist"] is True and tc["valid"] is True:
-        pass
-    elif tc["exist"] is True and tc["valid"] is False:
-        return ApiError.requestfail_error("token已过期")
-    elif tc["exist"] is False:
-        return ApiError.requestfail_error("token错误")
-    else:
-        return ApiError.requestfail_server("token校验失败")
-
-    # 4.校验账户所属角色是否具有后端权限
-    cr = ApiCheck.check_auth(
-        roleid=userdata["userRoleId"],
-        apiurl="getRolePermission.json"
-    )
-    if cr["exist"] is True and cr["pass"] is True:
-        pass
-    elif cr["exist"] is True and cr["pass"] is False:
-        return ApiError.requestfail_error("账户所属角色无访问权限")
-    elif cr["exist"] is False:
-        return ApiError.requestfail_error("账户所属角色不存在")
-    else:
-        return ApiError.requestfail_error("角色权限配置校验异常")
+    requestvalue_roleid = int(flask.request.args["role_id"])
 
     # 4.于数据库中查询角色配置清单
     try:
@@ -191,5 +111,4 @@ def get_role_permission():
     # 8.返回成功信息
     response_json["error_msg"] = "操作成功"
     # 最后返回内容
-    return json.dumps(response_json)
-
+    return response_json
