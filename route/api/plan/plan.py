64b1e5bb889f5172333a52c932068cf3d15ede2d
wufeng <wufeng@fclassroom.com>
2020-05-31 15:32:09 +0800

    feat(*): 接口restful化
    1.合并、重组当前所有api接口，转为restful规则：plan/getPersonalPlanInfo -> plan.json:get

--- route/api/plan/plan.py
+++ route/api/plan/plan.py
@@ -4,9 +4,17 @@
 from flask_restful import Resource
 
 
+from .restful_plan.get import plan_get
 from .restful_plan.post import plan_post
+from .restful_plan.delete import plan_delete
 
 
 class Plan(Resource):
     def post(self):
         return plan_post()
+
+    def delete(self):
+        return plan_delete()
+
+    def get(self):
+        return plan_get()
