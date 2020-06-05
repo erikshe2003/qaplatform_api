6c2421626dc50e4c3b98cc2e1f49e6cb2b09e2bb
wufeng <wufeng@fclassroom.com>
2020-06-05 16:01:49 +0800

    feat(*): 接口restful化
    1.合并、重组当前所有api接口，转为restful规则：task/stopTestTask -> task.json:put

--- route/api/task/stopTestTask/__init__.py
+++
@@ -1,3 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from .v0_1_4 import stop_test_task
--- route/api/task/task.py
+++ route/api/task/task.py
@@ -6,6 +6,7 @@ from flask_restful import Resource
 
 from .restful_task.get import task_get
 from .restful_task.post import task_post
+from .restful_task.put import task_put
 
 
 class Task(Resource):
@@ -14,3 +15,6 @@ class Task(Resource):
 
     def post(self):
         return task_post()
+
+    def put(self):
+        return task_put()
