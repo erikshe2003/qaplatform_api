4b9aeaaa86c421c3ab403b8aad5b9ec159ac1ded
wufeng <wufeng@fclassroom.com>
2020-06-11 16:51:00 +0800

    feat(*): 接口restful化
    1.合并、重组当前所有api接口，转为restful规则：test/json.json -> timeStamp.json:get

--- route/api/test/__init__.py
+++ route/api/test/__init__.py
@@ -1,11 +1,10 @@
 # -*- coding: utf-8 -*-
 
 from flask import Blueprint
+from flask_restful import Api
+
+from .time_stamp import TimeStamp
 
 api_test = Blueprint('api_test', __name__)
-
-# 加载具体路由
-from route.api.test.x_www_form_urlencoded import x_www_form_urlencoded
-from route.api.test.multipart_form_data import multipart_form_data
-from route.api.test.json import test_json
-from route.api.test.get import http_get
+api = Api(api_test)
+api.add_resource(TimeStamp, '/timeStamp.json')
---
+++ route/api/test/restful_time_stamp/get/__init__.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+
+from .v1_0_0 import time_stamp_get
