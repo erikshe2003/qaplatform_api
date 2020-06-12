78a706d33a8dd18306dbeeb2d8b72480f7dbd5f4
wufeng <wufeng@fclassroom.com>
2020-06-12 16:22:23 +0800

    feat(*): 接口restful化
    1.合并、重组当前所有api接口，转为restful规则：user/setBaseInfo -> userInfo.json:put

--- route/api/user/user_info.py
+++ route/api/user/user_info.py
@@ -5,8 +5,12 @@ from flask_restful import Resource
 
 
 from .restful_user_info.get import user_info_get
+from .restful_user_info.put import user_info_put
 
 
 class UserInfo(Resource):
     def get(self):
         return user_info_get()
+
+    def put(self):
+        return user_info_put()
