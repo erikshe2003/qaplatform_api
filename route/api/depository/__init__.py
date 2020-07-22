5e5fa859337ce182aa092f77d50785f6ec76fe17
zous <352319020@qq.com>
2020-07-22 10:19:13 +0800

    仓库接口包含depository和depositorys:前者支持get和post（需产id）请求，后者支持get

--- route/api/depository/__init__.py
+++ route/api/depository/__init__.py
@@ -2,10 +2,11 @@ from flask import Blueprint
 from flask_restful import Api
 
 from .depository import depository
-
+from .depositorys import depositorys
 
 api_depository = Blueprint('api_depository', __name__)
 
 api = Api(api_depository)
 
 api.add_resource(depository, '/depository.json')
+api.add_resource(depositorys, '/depositorys.json')
---
+++ route/api/depository/restful_depositorys/get/v1_0_1.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+
+import flask
+
+import route
+
+from handler.log import api_logger
+
+from model.mysql import model_mysql_depository
+
+"""
+    获取个人测试计划基础信息-api路由
+    ----校验
+            校验账户是否存在
+            校验账户操作令牌
+            校验账户所属角色是否有API操作权限
+            校验传参
+    ----操作
+
+            返回测试项目基础信息
+"""
+
+
+@route.check_user
+@route.check_token
+@route.check_auth
+def key_depositorys_get():
+    # 初始化返回内容
+    response_json = {
+        "code": 200,
+        "msg": "数据获取成功",
+        "data": []
+    }
+
+    # 查询项目信息基础信息
+    try:
+        mysql_depositorys_info = model_mysql_depository.query.filter().all()
+
+    except Exception as e:
+        api_logger.error("表model_mysql_subject读取失败，失败原因：" + repr(e))
+        return route.error_msgs[500]['msg_db_error']
+    else:
+        for mpti in mysql_depositorys_info:
+            response_json["data"].append({
+                "id": mpti.id,
+                "name": mpti.name,
+                "description": mpti.description,
+                "userId": mpti.userId,
+                "baseProjectId": mpti.baseProjectId,
+                "createTime": str(mpti.createTime)
+            })
+
+    # 最后返回内容
+    return response_json
