a9e9070e04d3ab647b037bd34624db1480b27ac5
zous <352319020@qq.com>
2020-07-23 18:41:15 +0800

    新增项目目录结构树构造和支持keyword搜索

--- route/api/project/__init__.py
+++ route/api/project/__init__.py
@@ -7,6 +7,7 @@ from .project_member import projectmember
 from .project_role import projectrole
 from .project_roleUser import projectroleUser
 from .project_list import projectlist
+from .project_column import projectcolumn
 api_project = Blueprint('api_project', __name__)
 
 api = Api(api_project)
@@ -17,3 +18,4 @@ api.add_resource(projectmember, '/member.json')
 api.add_resource(projectrole, '/role.json')
 api.add_resource(projectroleUser, '/roleUser.json')
 api.add_resource(projectlist, '/list.json')
+api.add_resource(projectcolumn, '/column.json')
