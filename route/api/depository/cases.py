d09e54b5f491511b374d74ad4c6ff1b37381966c
zous <352319020@qq.com>
2020-07-29 14:15:13 +0800

    新增仓库用例获取接口，支持关键词搜索

--- route/api/depository/cases.py
+++ route/api/depository/cases.py
@@ -3,7 +3,12 @@
 
 from flask_restful import Resource
 from .restful_cases.get import key_cases_get
-
+from .restful_cases.delete import key_cases_delete
+from .restful_cases.put import key_cases_put
 class cases(Resource):
     def get(self):
         return key_cases_get()
+    def delete(self):
+        return key_cases_delete()
+    def put(self):
+        return key_cases_put()
